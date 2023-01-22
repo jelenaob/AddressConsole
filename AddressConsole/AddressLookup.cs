0025184b0c2e2096f3980d45f84bf8576a91e113
unknown <jelenaobradovic.1993@yahoo.com>
2023-01-22 16:54:05 +0100

    Find possible addresses for certain address, console app.

---
+++ AddressConsole/AddressLookup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressConsole
+{
+    public class AddressLookup
+    {
+        public string Code { get; set; }
+        public string MemberId { get; set; }
+        public string Line1 { get; set; }
+        public string Postcode { get; set; }
+        public string Country { get; set; }
+        public string AddressId { get; set; }
+        public string OutputAddress { get; set; }
+        public bool SelectedFlag { get; set; }
+    }
+}
---
+++ AddressConsole/AddressResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressConsole
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace FindAdrTest
+    {
+        public class Item
+        {
+            public string Id { get; set; }
+            public string Type { get; set; }
+            public string Text { get; set; }
+            public string Highlight { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class AddressResponse
+        {
+            public List<Item> Items { get; set; }
+        }
+    }
+
+}
